321900401c02455a227c27c1f98cdf4517770eab
Richard Turner <richie.g.turner@gmail.com>
2016-10-05 10:08:35 -0400

    some random changes

--- models/user.js
+++
@@ -1,4 +0,0 @@
-//home || /login (login form)
-///profile/:id (display users profile)
-//summarys (list of summary models)
-//summarys/:id (individual summary)
--- server.js
+++ server.js
@@ -1,54 +1,80 @@
-//standard stuff
+// dependencies
 var express = require('express');
-var bodyParser = require('body-parser');
-var hbs = require('hbs');
-var methodOveride = require('method-override');
-var mongoose = require('mongoose');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var expressSession = require('express-session');
-var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var passport = require('passport');
-var passportLocal = require('passport-local');
-var passportLocalMongoose = require('passport-local-mongoose');
-var pry = require ('pryjs');
+var LocalStrategy = require('passport-local').Strategy;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
-app.set("view engine", "hbs");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(methodOveride('_method'));
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-  res.send("<h1>One Piece Weekly Discussion</h1>");
+
+app.use('/', routes);
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/passport_local_mongoose_express4');
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.get('/login', function(req, res){
-  res.send("<h1>One Piece Weekly Discussion</h1>");
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
-app.post('/blog', function(req, res){
-  res.send("<h1>One Piece Weekly Discussion</h1>");
-});
 
-app.post('/blog/:id', function(req, res){
-  res.send("<h1>One Piece Weekly Discussion</h1>");
-});
-
-// //log an error if db can't connect to mongodb
-// db.on('error', function(err){
-//   console.log(err);
-// });
-//
-// //log "database has been connected" if it successfully connects.
-// db.once('open', function(){
-//   console.log("database has been connected");
-// });
-
-
-
-
-app.listen(4000, function(){
-  console.log("listening on port 4000")
-});
+module.exports = app;
