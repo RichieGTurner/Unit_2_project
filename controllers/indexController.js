8ebb95eff105d3cac31bb462203def1716675c1a
Richard Turner <richie.g.turner@gmail.com>
2016-10-10 08:19:08 -0400

    small change

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,3 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 var Schema = require("../db/account.js");
+var Admin = Schema.Admin
+var User = Schema.User
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
